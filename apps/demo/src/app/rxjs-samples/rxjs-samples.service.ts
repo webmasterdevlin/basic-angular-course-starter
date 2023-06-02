6d000d50be68c0f959ad4cf45fea296d4f005068
devlinduldulao <webmasterdevlin@gmail.com>
2023-06-02 22:21:37 +0200

    update

--- apps/demo/src/app/rxjs-samples/rxjs-samples.service.ts
+++ apps/demo/src/app/rxjs-samples/rxjs-samples.service.ts
@@ -11,24 +11,29 @@ export class RxjsSamplesService {
   getPosts() {
     return this.http.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
   }
+
   getComments() {
     return this.http.get<Comment[]>(
       'https://jsonplaceholder.typicode.com/comments'
     );
   }
+
   getAlbums() {
     return this.http.get<Album[]>(
       'https://jsonplaceholder.typicode.com/albums'
     );
   }
+
   getPhotos() {
     return this.http.get<Photo[]>(
       'https://jsonplaceholder.typicode.com/photos'
     );
   }
+
   getTodos() {
     return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos');
   }
+
   getUsers() {
     return this.http.get<User[]>('https://jsonplaceholder.typicode.com/users');
   }
